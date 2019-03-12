12a9f0cea3113b25b6d69849065b5c3cad0c3fff
Gregory Heylen <gregoryheylen1998@gmail.com>
2019-03-12 22:26:50 +0100

    optellen an aftellen afgewerkt

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,32 @@
 $(document).ready(function() {
     init()
     
+
+    function toonTeams() {
+        $('#teams').empty()
+        if(localStorage.getItem('team') != null) {
+            array = JSON.parse(localStorage.getItem('team'))
+            for(var team in array) {
+                var score = array[team]
+                $('#teams').append('<div class="col l3 offset-l"> \
+                <div class="card-panel white"> \
+                    <span> \
+                        <p>' + team + '</p> \
+                        <div class="valign-wrapper"> \
+                            <div class="col l12 valign-wrapper"> \
+                                <button value="' + team + '" class="btn-flat border center col l3 substractScore">-</button> \
+                                <input id="' + team + '" class="col l4 browser-default border offset-l1" type="text"> \
+                                <button value="' + team + '" class="btn-flat border center col l3 offset-l1 addScore">+</button> \
+                            </div> \
+                        </div> \
+                        <p>Score: ' + score + '</p> \
+                    </span> \
+                </div> \
+                </div>')
+            }
+        }
+    }
+    
     $('#addTeam').on("click", function() {        
         var naam = $('#teamNaamNieuw').val();
         var array = {}
@@ -14,47 +40,44 @@ $(document).ready(function() {
         } else {
             console.log('error')
         }
+        location.reload()
     })
 
     $('#removeAllTeams').on("click", function() {
         localStorage.clear();
+        location.reload()
     })
 
-    $('#addScore').on("click", function() {
-        console.log('test');
-
+    $('.addScore').on("click", function() {
+        var team = $(this).val();
+        var toeTeVoegenScore = parseInt($('#' + team).val())
+        console.log(toeTeVoegenScore)
+        if(toeTeVoegenScore === '' || Number.isInteger(toeTeVoegenScore) == false){
+        } else {
+            array = JSON.parse(localStorage.getItem('team'))
+            huidigeScore = array[team]
+            array[team] = parseInt(huidigeScore) + parseInt(toeTeVoegenScore)
+            localStorage.setItem('team' , JSON.stringify(array));
+        }
+        location.reload()
     })
 
-    $('#substractScore').on("click", function() {
-        console.log('test');
-
+    $('.substractScore').on("click", function() {
+        var team = $(this).val();
+        var toeTeVoegenScore = parseInt($('#' + team).val())
+        console.log(toeTeVoegenScore)
+        if(toeTeVoegenScore === '' || Number.isInteger(toeTeVoegenScore) == false){
+        } else {
+            array = JSON.parse(localStorage.getItem('team'))
+            huidigeScore = array[team]
+            array[team] = parseInt(huidigeScore) - parseInt(toeTeVoegenScore)
+            localStorage.setItem('team' , JSON.stringify(array));
+        }
+        location.reload()
     })
 
     function init() {
         console.log('klaar voor gebruik')
-        if(localStorage.getItem('team') != null) {
-            array = JSON.parse(localStorage.getItem('team'))
-            for(var team in array) {
-                var score = array[team]
-                $('#teams').append('<div class="col l3 offset-l"> \
-                <div class="card-panel white"> \
-                    <span> \
-                        <p>' + team + '</p> \
-                        <div class="valign-wrapper"> \
-                            <div class="col l12 valign-wrapper"> \
-                                <a id="addScore" value="' + team + '" class="btn-flat border center col l3">-</a> \
-                                <input id="' + team + '" class="col l4 browser-default border offset-l1" type="number"> \
-                                <a id="substractScore"value="' + team + '" class="btn-flat border center col l3 offset-l1 1">+</a> \
-                            </div> \
-                        </div> \
-                        <p>Score: ' + score + '</p> \
-                    </span> \
-                </div> \
-                </div>')
-            }
-        }
+        toonTeams()
     }
 })
-
-
-{/*  */}
